ab58360bd8335e6bc79d5949c8f5b3790a02363a
Sparsh <99.sparsh@gmail.com>
2019-11-03 19:28:08 +0530

    schema updated

--- oyo_create.sql
+++ oyo_create.sql
@@ -1,109 +1,329 @@
-CREATE TABLE `users` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL UNIQUE,
-	`password` varchar(255) NOT NULL,
-	`phone` varchar(10) NOT NULL UNIQUE,
-	`dob` DATE NOT NULL,
-	`prime` BINARY(1) NOT NULL,
-	PRIMARY KEY (`id`)
-);
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 03, 2019 at 07:20 PM
+-- Server version: 5.7.27-0ubuntu0.16.04.1
+-- PHP Version: 7.0.33-0ubuntu0.16.04.7
 
-CREATE TABLE `hotels` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`address` varchar(255) NOT NULL,
-	`city` varchar(20) NOT NULL,
-	`pincode` INT(6) NOT NULL,
-	`admin_id` INT NOT NULL,
-	PRIMARY KEY (`id`)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-CREATE TABLE `hotel_admin` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`password` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-);
 
-CREATE TABLE `rooms` (
-	`id` INT NOT NULL,
-	`type` varchar(20) NOT NULL,
-	PRIMARY KEY (`id`)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `room_availability` (
-	`hotel_id` INT NOT NULL,
-	`room_id` INT NOT NULL,
-	`quantity` INT NOT NULL,
-	PRIMARY KEY (`hotel_id`,`room_id`)
-);
+--
+-- Database: `oyo`
+--
+CREATE DATABASE IF NOT EXISTS `oyo` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `oyo`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `amenities`
+--
 
 CREATE TABLE `amenities` (
-	`hotel_id` INT NOT NULL,
-	`room_id` INT NOT NULL,
-	`pool` BINARY NOT NULL,
-	`room_service` BINARY NOT NULL,
-	`housekeeping` BINARY NOT NULL,
-	`wifi` BINARY NOT NULL,
-	`cab_service` BINARY NOT NULL,
-	`parking` BINARY NOT NULL,
-	`banquet` BINARY NOT NULL,
-	`AC` BINARY NOT NULL,
-	`geyser` BINARY NOT NULL,
-	`mini_bar` BINARY NOT NULL,
-	`spa` BINARY NOT NULL,
-	`gym` BINARY NOT NULL,
-	`recreation_center` BINARY NOT NULL,
-	PRIMARY KEY (`hotel_id`,`room_id`)
-);
+  `hotel_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `pool` binary(1) NOT NULL,
+  `room_service` binary(1) NOT NULL,
+  `housekeeping` binary(1) NOT NULL,
+  `wifi` binary(1) NOT NULL,
+  `cab_service` binary(1) NOT NULL,
+  `parking` binary(1) NOT NULL,
+  `banquet` binary(1) NOT NULL,
+  `AC` binary(1) NOT NULL,
+  `geyser` binary(1) NOT NULL,
+  `mini_bar` binary(1) NOT NULL,
+  `spa` binary(1) NOT NULL,
+  `gym` binary(1) NOT NULL,
+  `recreation_center` binary(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `amenities`
+--
+
+INSERT INTO `amenities` (`hotel_id`, `room_id`, `pool`, `room_service`, `housekeeping`, `wifi`, `cab_service`, `parking`, `banquet`, `AC`, `geyser`, `mini_bar`, `spa`, `gym`, `recreation_center`) VALUES
+(1, 1, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31, 0x31);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
 
 CREATE TABLE `bookings` (
-	`user_id` INT NOT NULL,
-	`hotel_id` INT NOT NULL,
-	`room_id` INT NOT NULL,
-	`checkin` DATE NOT NULL,
-	`checkout` DATE NOT NULL,
-	`bill_amount` INT NOT NULL,
-	PRIMARY KEY (`user_id`,`hotel_id`,`room_id`,`checkin`,`checkout`)
-);
+  `user_id` int(11) NOT NULL,
+  `hotel_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `checkin` date NOT NULL,
+  `checkout` date NOT NULL,
+  `bill_amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotels`
+--
+
+CREATE TABLE `hotels` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `pincode` int(6) NOT NULL,
+  `admin_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hotels`
+--
+
+INSERT INTO `hotels` (`id`, `name`, `address`, `city`, `pincode`, `admin_id`) VALUES
+(1, 'ITC', 'Dwarka', 'Delhi', 400002, 1),
+(2, 'End Point', 'Country Inn', 'Manipal', 576104, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotel_admin`
+--
+
+CREATE TABLE `hotel_admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hotel_admin`
+--
+
+INSERT INTO `hotel_admin` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Sparsh', '99.sparsh@gmail.com', 'sparsh123'),
+(2, 'test', 'tester', 'test123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pricing`
+--
 
 CREATE TABLE `pricing` (
-	`price` INT NOT NULL,
-	`hotel_id` INT NOT NULL,
-	`room_id` INT NOT NULL,
-	PRIMARY KEY (`hotel_id`,`room_id`)
-);
+  `price` int(11) NOT NULL,
+  `hotel_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
 
 CREATE TABLE `reviews` (
-	`user_id` INT NOT NULL,
-	`hotel_id` INT NOT NULL,
-	`review` TEXT NOT NULL,
-	`rating` INT NOT NULL DEFAULT '1',
-	PRIMARY KEY (`user_id`,`hotel_id`)
-);
+  `user_id` int(11) NOT NULL,
+  `hotel_id` int(11) NOT NULL,
+  `review` text NOT NULL,
+  `rating` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `hotels` ADD CONSTRAINT `hotels_fk0` FOREIGN KEY (`admin_id`) REFERENCES `hotel_admin`(`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `room_availability` ADD CONSTRAINT `room_availability_fk0` FOREIGN KEY (`hotel_id`) REFERENCES `hotels`(`id`);
+--
+-- Table structure for table `rooms`
+--
 
-ALTER TABLE `room_availability` ADD CONSTRAINT `room_availability_fk1` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`);
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `amenities` ADD CONSTRAINT `amenities_fk0` FOREIGN KEY (`hotel_id`) REFERENCES `hotels`(`id`);
+--
+-- Dumping data for table `rooms`
+--
 
-ALTER TABLE `amenities` ADD CONSTRAINT `amenities_fk1` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`);
+INSERT INTO `rooms` (`id`, `type`) VALUES
+(1, 'Standard'),
+(2, 'Deluxe'),
+(3, 'Suite');
 
-ALTER TABLE `bookings` ADD CONSTRAINT `bookings_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `bookings` ADD CONSTRAINT `bookings_fk1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels`(`id`);
+--
+-- Table structure for table `room_availability`
+--
 
-ALTER TABLE `bookings` ADD CONSTRAINT `bookings_fk2` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`);
+CREATE TABLE `room_availability` (
+  `hotel_id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `pricing` ADD CONSTRAINT `pricing_fk0` FOREIGN KEY (`hotel_id`) REFERENCES `hotels`(`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `pricing` ADD CONSTRAINT `pricing_fk1` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`);
+--
+-- Table structure for table `users`
+--
 
-ALTER TABLE `reviews` ADD CONSTRAINT `reviews_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `dob` date NOT NULL,
+  `prime` binary(1) NOT NULL,
+  `access` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `reviews` ADD CONSTRAINT `reviews_fk1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels`(`id`);
+--
+-- Dumping data for table `users`
+--
 
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `dob`, `prime`, `access`) VALUES
+(1, 'ab', 'cd', 'abcd', 'ef', '2012-08-09', 0x30, 0),
+(2, 'Sparsh', '99.sparsh@gmail.com', 'sparsh123', '8100403026', '1999-08-02', 0x30, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `amenities`
+--
+ALTER TABLE `amenities`
+  ADD PRIMARY KEY (`hotel_id`,`room_id`),
+  ADD KEY `amenities_fk1` (`room_id`);
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`user_id`,`hotel_id`,`room_id`,`checkin`,`checkout`),
+  ADD KEY `bookings_fk1` (`hotel_id`),
+  ADD KEY `bookings_fk2` (`room_id`);
+
+--
+-- Indexes for table `hotels`
+--
+ALTER TABLE `hotels`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `hotels_fk0` (`admin_id`);
+
+--
+-- Indexes for table `hotel_admin`
+--
+ALTER TABLE `hotel_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pricing`
+--
+ALTER TABLE `pricing`
+  ADD PRIMARY KEY (`hotel_id`,`room_id`),
+  ADD KEY `pricing_fk1` (`room_id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`user_id`,`hotel_id`),
+  ADD KEY `reviews_fk1` (`hotel_id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room_availability`
+--
+ALTER TABLE `room_availability`
+  ADD PRIMARY KEY (`hotel_id`,`room_id`),
+  ADD KEY `room_availability_fk1` (`room_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phone` (`phone`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `hotels`
+--
+ALTER TABLE `hotels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `hotel_admin`
+--
+ALTER TABLE `hotel_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `amenities`
+--
+ALTER TABLE `amenities`
+  ADD CONSTRAINT `amenities_fk0` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`id`),
+  ADD CONSTRAINT `amenities_fk1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_fk1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`id`),
+  ADD CONSTRAINT `bookings_fk2` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+--
+-- Constraints for table `hotels`
+--
+ALTER TABLE `hotels`
+  ADD CONSTRAINT `hotels_fk0` FOREIGN KEY (`admin_id`) REFERENCES `hotel_admin` (`id`);
+
+--
+-- Constraints for table `pricing`
+--
+ALTER TABLE `pricing`
+  ADD CONSTRAINT `pricing_fk0` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`id`),
+  ADD CONSTRAINT `pricing_fk1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_fk1` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`id`);
+
+--
+-- Constraints for table `room_availability`
+--
+ALTER TABLE `room_availability`
+  ADD CONSTRAINT `room_availability_fk0` FOREIGN KEY (`hotel_id`) REFERENCES `hotels` (`id`),
+  ADD CONSTRAINT `room_availability_fk1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/oyo/DbConnection.java
+++ src/oyo/DbConnection.java
@@ -21,7 +21,7 @@ public class DbConnection {
         String db="",user="",pass="";
         try{
             FileInputStream fis=new FileInputStream("/home/sparsh/Documents/RUP Lab OYO/oyo/src/oyo/dbsettings.properties"); 
-            Properties p=new Properties (); 
+            Properties p=new Properties(); 
             p.load(fis); 
             db=(String)p.getProperty("db");
             user=(String)p.getProperty("user");
