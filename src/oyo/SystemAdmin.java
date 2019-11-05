20b857710430465c5eec85bac62245993fea9227
Sparsh <99.sparsh@gmail.com>
2019-11-05 12:51:29 +0530

    delete hotel added

--- src/oyo/SystemAdmin.java
+++ src/oyo/SystemAdmin.java
@@ -54,6 +54,11 @@ public class SystemAdmin extends javax.swing.JFrame {
         });
 
         jButton3.setText("Delete Hotel");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -89,6 +94,10 @@ public class SystemAdmin extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         new VeiwHotels().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        new DeleteHotel().setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
